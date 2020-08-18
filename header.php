e05d8a8f0c499cbced78bba05b6637e8adccab5d
harada <aya33798@gmail.com>
2020-08-18 15:15:13 +0900

    テンプレート内条件分岐書き方修正

--- header.php
+++ header.php
@@ -3,21 +3,19 @@
     <h1><a href="index.php">SportMatching</a></h1>
     <nav class="top_nav">
       <ul>
-        <?php
-          if(empty($_SESSION['user_id'])){
-        ?>
-            <li><a href="user_register.php">ユーザー登録</a></li>
-            <li><a href="login.php">ログイン</a></li>
-        <?php
-          }else{
-        ?>
-            <li><a href="index.php">一覧</a></li>
-            <li><a href="like.php?status=like">Like</a></li>
-            <li><a href="chatIndex.php">チャット</a></li>
-            <li><a href="logout.php">ログアウト</a></li>
-        <?php
-          }
-        ?>
+        <?php if(empty($_SESSION['user_id'])): ?>
+
+          <li><a href="user_register.php">ユーザー登録</a></li>
+          <li><a href="login.php">ログイン</a></li>
+
+        <?php else: ?>
+
+          <li><a href="index.php">一覧</a></li>
+          <li><a href="like.php?status=like">Like</a></li>
+          <li><a href="chatIndex.php">チャット</a></li>
+          <li><a href="logout.php">ログアウト</a></li>
+
+        <?php endif; ?>
       </ul>
     </nav>
   </div>
